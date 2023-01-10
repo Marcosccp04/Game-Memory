f62296a909e918cfadb8598c18135ddcd55fa29d
Marcos Augusto <marcos.augusto@icomp.ufam.edu.br>
2023-01-10 18:51:33 -0400

    corrigindo as imagens

--- scripts/script.js
+++ scripts/script.js
@@ -22,7 +22,7 @@ function populandoMosaico(listaCartas) {
   vetor = vetor.sort((a,b) => Math.random()-0.5);
   for (let contador = 0; contador < vetor.length; contador++) {
     console.log(contador);
-    mosaicoVerso.innerHTML += `<img class="carta carta${contador}" onclick="mudarVerso(${contador})" src="/img/${vetor[contador]}" alt="A gata"></img>`;
+    mosaicoVerso.innerHTML += `<img class="carta carta${contador}" onclick="mudarVerso(${contador})" src="./img/${vetor[contador]}" alt="A gata"></img>`;
   }
 }
 populandoMosaico();
@@ -31,11 +31,11 @@ function mudarVerso(cardId) {
   const cartaVerso = document.querySelector(".cartas-verso .carta"+cardId);
   cardAtual = cartaVerso.attributes.getNamedItem('src');
   if(cardAtual !== cardSrc){
-    cartaVerso.setAttribute('src', `/img/${vetor[cardId]}`);
+    cartaVerso.setAttribute('src', `./img/${vetor[cardId]}`);
     cardSrc = cardAtual;
   }else{
     cardSrc = null;
-    cartaVerso.setAttribute('src', `/img/CardBack.jpg`);
+    cartaVerso.setAttribute('src', `./img/CardBack.jpg`);
   }
 
 }
