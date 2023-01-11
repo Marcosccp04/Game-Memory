565873c3482a272727baf5e7f75a0b557c1d4711
Marcos Augusto <marcos.augusto@icomp.ufam.edu.br>
2023-01-10 23:45:13 -0400

    adicionando a logica do jogo

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,10 @@
 let cardSrc;
-let quantidadeCartas = 1;
-
+let quantidadeCartas
 let vetor;
+let  carta1 = null;
+let carta2 = null;
+let carta1Id = null;
+let cartaString;
 
 function numeroCartas() {   
     let cardsArray = ["202.webp"];
@@ -22,7 +25,7 @@ function populandoMosaico(listaCartas) {
   vetor = vetor.sort((a,b) => Math.random()-0.5);
   for (let contador = 0; contador < vetor.length; contador++) {
     console.log(contador);
-    mosaicoVerso.innerHTML += `<img class="carta carta${contador}" onclick="mudarVerso(${contador})" src="./img/${vetor[contador]}" alt="A gata"></img>`;
+    mosaicoVerso.innerHTML += `<img class="carta carta${contador}" onclick="mudarVerso(${contador})" src="../img/CardBack.jpg" alt="A gata"></img>`;
   }
 }
 populandoMosaico();
@@ -31,11 +34,35 @@ function mudarVerso(cardId) {
   const cartaVerso = document.querySelector(".cartas-verso .carta"+cardId);
   cardAtual = cartaVerso.attributes.getNamedItem('src');
   if(cardAtual !== cardSrc){
-    cartaVerso.setAttribute('src', `./img/${vetor[cardId]}`);
+    cartaVerso.setAttribute('src', `../img/${vetor[cardId]}`);
+    cartaString = cartaVerso.attributes.getNamedItem('src').value;
     cardSrc = cardAtual;
+    console.log(carta1,carta2);
+    if(carta1 === null){
+      carta1 = cartaString;
+      carta1Id = cardId;
+    }else{
+      if(carta1Id != cardId){
+        carta2 = cartaString;
+      }
+    }
+    if(carta2 != null){
+      const cartaVerso1 = document.querySelector(".cartas-verso .carta"+carta1Id);
+      if(carta1 == carta2){
+        console.log("deu match");
+       cartaVerso.classList.add("match");
+       cartaVerso1.classList.add("match");
+       cartaVerso.setAttribute('onclick','#');
+       cartaVerso1.setAttribute('onclick','#');
+      }else{
+        setTimeout(() => {cartaVerso.setAttribute('src', `../img/CardBack.jpg`)},1000);
+        setTimeout(() =>{cartaVerso1.setAttribute('src', `../img/CardBack.jpg`)},1000);
+      }
+      carta1 = null;
+      carta2 = null;
+    }
   }else{
     cardSrc = null;
-    cartaVerso.setAttribute('src', `./img/CardBack.jpg`);
+    cartaVerso.setAttribute('src', `../img/CardBack.jpg`);
   }
-
 }
